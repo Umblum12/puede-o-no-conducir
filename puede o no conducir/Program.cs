e4a3309091022317691ec5ae66906619ced167e6
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-25 03:38:16 -0700

    Agregar archivos de proyecto.

---
+++ puede o no conducir/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace puede_o_no_conducir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vamos a evaluar si puedes conducir un veiculo");
+            Console.WriteLine("Introduce tu edad, por favor");
+            int edad = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("¿Tienes carnet?");
+            string carnet = Console.ReadLine();
+
+            if (edad <= 17)
+            {
+                Console.WriteLine("No puedes conducir chamaco pendejo");
+            }
+
+
+            if (edad >=18 && carnet=="si")
+            {
+                Console.WriteLine("Puedes conducir vehiculos");
+            }
+            else 
+            {
+                Console.WriteLine("No puedes conducir vehiculos");
+            }
+        }
+    }
+}
